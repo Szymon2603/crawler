6aa54fb6fb7715824659642a3a56c8a49f60e697
Szymon Grzleak <szymong333@gmail.com>
2017-05-28 18:31:26 +0200

    Zadanie #17 Dodanie CORS

--- crawler-app/src/main/java/pl/beardeddev/crawler/app/config/WebConfig.java
+++ crawler-app/src/main/java/pl/beardeddev/crawler/app/config/WebConfig.java
@@ -28,6 +28,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -52,4 +53,10 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
         configurer.defaultContentType(MediaType.APPLICATION_JSON);
     }
+    
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+    }
+    
 }
