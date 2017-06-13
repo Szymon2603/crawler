a54b1f2f0798768d13aae449663e8fcddf997fdb
Szymon Grzleak <szymong333@gmail.com>
2017-06-13 20:56:29 +0200

    Zadanie #35 Usprawnienie warstwy serwisowej

--- crawler-client/src/app/crawler/crawler.service.ts
+++ crawler-client/src/app/crawler/crawler.service.ts
@@ -1,15 +1,16 @@
 import { Injectable } from '@angular/core';
-import { Http, Response, URLSearchParams } from '@angular/http';
+import { Http, URLSearchParams } from '@angular/http';
 import { Image } from '../models/image';
 import { ConfigPackageMaster } from '../models/config-package-master';
 import { Observable } from 'rxjs/Observable';
+import { ServiceBase } from '../service/basic-service';
 
 @Injectable()
-export class CrawlerService {
+export class CrawlerService extends ServiceBase {
 
     runCrawlerUrl = 'http://localhost:8080/crawler-app/runCrawler'
 
-    constructor(private http: Http) { }
+    constructor(private http: Http) { super() }
 
     runCrawler(startUrl: string, configId: number, maxVisits?: number): Observable<Image[]> {
         let params = this.prepareParams(startUrl, configId, maxVisits);
@@ -27,24 +28,6 @@ export class CrawlerService {
             params.set('maxVisits', maxVisits.toString());
         }
         return params;
-    }
-
-    private extractData(res: Response) {
-        let body = res.json();
-        return body.data || {};
-    }
-
-    private handleError(error: Response | any) {
-        let errMsg: string;
-        if (error instanceof Response) {
-            const body = error.json() || '';
-            const err = body.error || JSON.stringify(body);
-            errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
-        } else {
-            errMsg = error.message ? error.message : error.toString();
-        }
-        console.error(errMsg);
-        return Observable.throw(errMsg);
     }
 
     /**
--- crawler-client/src/app/image/image.service.ts
+++ crawler-client/src/app/image/image.service.ts
@@ -2,11 +2,12 @@ import { Injectable } from '@angular/core';
 import { Http, Response, URLSearchParams } from '@angular/http';
 import { Image } from '../models/image';
 import { Observable } from 'rxjs/Observable';
+import { ServiceBase } from '../service/basic-service';
 
 @Injectable()
-export class ImageService {
+export class ImageService extends ServiceBase {
 
-    constructor(private http: Http) { }
+    constructor(private http: Http) { super() }
 
     imagesEndpoint = 'http://localhost:8080/crawler-app/images';
 
@@ -15,23 +16,5 @@ export class ImageService {
             .get(this.imagesEndpoint)
             .map(this.extractData)
             .catch(this.handleError)
-    }
-
-    private extractData(res: Response) {
-        let body = res.json();
-        return body.data || {};
-    }
-
-    private handleError(error: Response | any) {
-        let errMsg: string;
-        if (error instanceof Response) {
-            const body = error.json() || '';
-            const err = body.error || JSON.stringify(body);
-            errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
-        } else {
-            errMsg = error.message ? error.message : error.toString();
-        }
-        console.error(errMsg);
-        return Observable.throw(errMsg);
     }
 }
