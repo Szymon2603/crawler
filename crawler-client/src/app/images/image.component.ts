9fe7d1e6996c11a5f4169138d6f975530c936b42
Szymon Grzleak <szymong333@gmail.com>
2017-06-19 22:44:17 +0200

    Zadanie #35 Dodawanie obrazka z poziomu listy obrazków (na podstawie braku id)

--- crawler-client/src/app/images/image.component.ts
+++ crawler-client/src/app/images/image.component.ts
@@ -1,11 +1,25 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, AfterContentInit } from '@angular/core';
 import { Image } from './../models/image';
+import { ImagesService } from './images.service';
 
 @Component({
     selector: 'image',
     templateUrl: 'image.component.html',
     styleUrls: ['image.component.scss']
 })
-export class ImageComponent {
+export class ImageComponent implements AfterContentInit {
     @Input() image: Image;
+    isNew: boolean;
+
+    constructor(private imagesService: ImagesService) { }
+
+    ngAfterContentInit() {
+        this.isNew = this.image.id == undefined || this.image.id == null;
+    }
+
+    add() {
+        console.log('Dodaję ' + this.image.toString())
+        this.imagesService.addImageMock(this.image);
+        this.isNew = false;
+    }
 }
--- crawler-client/src/app/images/images.service.ts
+++ crawler-client/src/app/images/images.service.ts
@@ -7,28 +7,49 @@ import { ServiceBase } from '../commons/services/basic-service';
 @Injectable()
 export class ImagesService extends ServiceBase {
 
-    constructor(private http: Http) { super() }
+    constructor(private http: Http) {
+        super();
+        this.initImagesMockList();
+    }
 
     imagesEndpoint = 'http://localhost:8080/crawler-app/images';
 
-    images() : Observable<Image[]> {
+    images(): Observable<Image[]> {
         return this.http
             .get(this.imagesEndpoint)
             .map(this.extractData)
             .catch(this.handleError)
     }
 
+    private imagesMockList: Image[] = [];
+
+    private initImagesMockList(): void {
+        if(this.imagesMockList.length == 0) {
+            let imageURL = 'https://img-9gag-fun.9cache.com/photo/a6bWMX8_700b.jpg';
+            for(let i = 0; i < 10; ++i) {
+                this.imagesMockList.push(new Image(i + 1, imageURL, i + 10, i + 20, new Date(Date.now())));
+            }
+        }
+    }
+
     /**
     * Atrapa na potrzeby testów
     */
-    imagesMock() : Observable<Image[]> {
-        let images: Image[] = [];
-        let imageURL = 'https://img-9gag-fun.9cache.com/photo/a6bWMX8_700b.jpg';
-        for(let i = 0; i < 10; ++i) {
-            images.push(new Image(i + 1, imageURL, i + 10, i + 20, new Date(Date.now())));
-        }
+    imagesMock(): Observable<Image[]> {
         return Observable
-            .from([images])
-            .delay(images.length * 100);
+            .from([this.imagesMockList])
+            .delay(this.imagesMockList.length * 10);
+    }
+
+    /**
+    * Atrapa na potrzeby testów
+    */
+    addImageMock(image: Image): void {
+        let ids: number[] = this.imagesMockList.map(v => {
+            return v.id;
+        })
+        let lastId = Math.max.apply(ids);
+        image.id = lastId;
+        this.imagesMockList.push(image);
     }
 }
